06810279f63004b19ae3d6a9a5fccbdde06d067b
Eric Carlson <carlson@hyades.ucsc.edu>
2015-11-13 15:37:34 -0800

    incremental update

---
+++ Transfer.sh
@@ -0,0 +1,22 @@
+# cd ..//galprop/output/
+# scp mod_s_42_XCO_P8.hdf5 planck:/data/GCE_sys/new & 
+# scp mod_s_46_XCO_P8.hdf5 planck:/data/GCE_sys/new 
+# cd ../../GCE_sys/
+
+
+cd ..//galprop/output/
+scp mod_s_44_XCO_P8_corrected.hdf5 planck:/data/GCE_sys/new & 
+#scp mod_s_47_XCO_P8_corrected.hdf5 planck:/data/GCE_sys/new 
+cd ../../GCE_sys/
+
+#cd ..//galprop/output/
+# scp mod_sMS04_42_XCO_P8.hdf5 planck:/data/GCE_sys/new & 
+# scp mod_sMS04_46_XCO_P8.hdf5 planck:/data/GCE_sys/new 
+
+#for i_model in {43,47,48}
+#do
+#	scp mod_sMS04_"$i_model"_XCO_P8.hdf5 planck:/data/GCE_sys/new 
+#	scp mod_sMS04_"$i_model"_XCO_P8.hdf5 planck:/data/GCE_sys/new 
+#done 
+#cd ../../GCE_sys/
+
